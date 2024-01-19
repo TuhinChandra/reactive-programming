bb7f2a1b6a3fe59c67536e1a0324d4448086d53c
chandt02 <tuhin.chandra@kingfisher.com>
2024-01-19 02:12:33 +0000

    Reactive Programming with FLux and Mono

--- src/test/java/reactive/programming/AppTest.java
+++ src/test/java/reactive/programming/AppTest.java
@@ -4,11 +4,9 @@
 package reactive.programming;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        Main classUnderTest = new Main();
     }
 }
